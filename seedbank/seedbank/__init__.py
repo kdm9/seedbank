fc8bcd9eaf24adbc9a5c2f3ad93ca31559fff273
Kevin Murray <k.d.murray.91@gmail.com>
2013-01-26 21:59:53 +1100

    init.py up to 'demo' stage with collections model

--- seedbank/seedbank/__init__.py
+++ seedbank/seedbank/__init__.py
@@ -1,11 +1,14 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from .models import (
-    DBSession,
-    Base,
-    )
+from seedbank.models import (
+        DBSession,
+        Base,
+        )
 
+from seedbank.views import (
+        CollectionView,
+        )
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -14,7 +17,18 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
+
+    # Static route (to static files)
     config.add_static_view('static', 'static', cache_max_age=3600)
+
+    # Root-level routes
     config.add_route('home', '/')
+    
+    # Routes for models.Collection
+    config.add_route('collection_index', "/collections/$")
+    config.add_route('collection_add', "/collections/add/$")
+    config.add_route('collection_view', "/collections/{voucher}/")
+
+    # Make the wsgi app
     config.scan()
     return config.make_wsgi_app()
